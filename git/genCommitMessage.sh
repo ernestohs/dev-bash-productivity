774f323dbccd273e884a3718a801f0e64ab6f1aa
Ernesto Herrera <ernestohs@gmail.com>
2024-06-24 12:54:24 -0600

    fix(git):Fix parameters for the generator of commit messages

--- git/genCommitMessage.sh
+++ git/genCommitMessage.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 TEMP_DIFF_FILE="/tmp/$(uuidgen).tmp.diff"
 git diff --cached > $TEMP_DIFF_FILE
 
 PROMPT="Review the following \`git diff --cached\` output and write a concise commit message summarizing the changes:"
-COMMIT_MSG=$(cat $TEMP_DIFF_FILE | ollama run -m ollama3 -p $PROMPT)
-
+COMMIT_MSG=$(ollama run llama3 "$PROMPT $(cat $TEMP_DIFF_FILE)" )
+echo "$COMMIT_MSG"
 rm $TEMP_DIFF_FILE
-temp_file=$(mktemp)
-echo "$COMMIT_MSG" > "$temp_file"
-
-git commit -F "$temp_file"
+echo "$COMMIT_MSG"
